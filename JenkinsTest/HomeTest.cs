240d1c5a625c04dbb3acbdc4f3fdf7b518f8fd63
Ritesh Kumar <me.ritesh.kumar@gmail.com>
2020-07-10 10:15:15 +0530

    Commit - Unit Test

---
+++ JenkinsTest/HomeTest.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JenkinsTest
+{
+    [TestClass]
+    public class HomeTest
+    {
+        [TestMethod]
+        public void IsIndexRunning()
+        {
+            var input = "RUNNING";
+
+            Assert.AreEqual("RUNNING", input);
+        }
+    }
+}
